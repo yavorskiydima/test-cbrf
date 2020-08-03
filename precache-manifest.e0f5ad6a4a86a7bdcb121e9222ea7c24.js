e0bac7af9a568fabd4cf50e7f95a422a6d3b824c
d.yavorskiy <d.yavorskiy@eastwind.ru>
2020-08-03 11:36:01 +0500

    Updates

---
+++ precache-manifest.e0f5ad6a4a86a7bdcb121e9222ea7c24.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f799a2f1913becff434f",
+    "url": "/static/js/main.f799a2f1.chunk.js"
+  },
+  {
+    "revision": "588b58f9e689b819ba54",
+    "url": "/static/js/1.588b58f9.chunk.js"
+  },
+  {
+    "revision": "a90dfeeb3e0b4bee1e98602a9e57ee52",
+    "url": "/index.html"
+  }
+];
